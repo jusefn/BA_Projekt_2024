9a078e6a0b14f0c28a71c1fd96406d1eba79c3eb
Jusef N. <2770770+jusefn@users.noreply.github.com>
2024-11-22 19:39:47 +0100

    Revert "added JavaFX Maven Project"
    This reverts commit 152382240aba2da27b9d9d9e67763d812aa28ee8.

---
+++ csharpapp/AppClassLibrary/Handlers/FileHandler.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using System.Text.Json;
+using AppClassLibrary.Enums;
+using AppClassLibrary.Models;
+
+namespace AppClassLibrary.Handlers
+{
+    public static class FileHandler
+    {
+
+        public static void SaveFile(string content, string path, ContentTypeEnum type)
+        {
+            FileObject fileObject = new FileObject();
+            fileObject.ContentType = type;
+            fileObject.Content = content;
+            SaveToFS(fileObject, path);
+        }
+
+        private static void SaveToFS(FileObject fileObject, string path)
+        {
+            using (var stream = File.Open(path, FileMode.Create))
+            {
+                using (var writer = new BinaryWriter(stream, Encoding.UTF8, false))
+                {
+                    writer.Write(JsonSerializer.SerializeToUtf8Bytes(fileObject));
+                }
+            }
+        }
+
+        public static string LoadFile(string path, ContentTypeEnum type)
+        {
+            FileObject fileObject = LoadFromFS(path);
+            if(fileObject.ContentType != type)
+            {
+                throw new Exception("Content Type is not equal.");
+            }
+            if (fileObject.Content == null)
+            {
+                throw new Exception("Content should not be null.");
+            }
+            return fileObject.Content;
+        }
+
+        private static FileObject LoadFromFS(string path)
+        {
+            if (File.Exists(path))
+            {
+                using (var stream = File.Open(path, FileMode.Open))
+                {
+                    using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
+                    {
+                        byte[] contentBytes = reader.ReadBytes((int)stream.Length);
+                        var readOnlySpan = new ReadOnlySpan<byte>(contentBytes);
+
+                        return JsonSerializer.Deserialize<FileObject>(readOnlySpan)!;
+
+                    }
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException("File not found");
+            }
+        }
+    }
+}
---
+++ csharpapp/wpf/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using AppClassLibrary.Enums;
+using AppClassLibrary.Handlers;
+using AppClassLibrary.Models;
+using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using wpf.interfaces;
+using wpf.tabItems;
+
+namespace wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if(fileDialog.ShowDialog() == true)
+            {
+                string path = fileDialog.FileName;
+                ContentTypeEnum contentTypeEnum = ContentTypeEnum.NOTES;
+                if (tabs.SelectedContent.GetType().Name == "Calculator")
+                {
+                    contentTypeEnum = ContentTypeEnum.CALCULATOR;
+                }
+
+                try
+                {
+                    if (tabs.SelectedContent is IContentProvider contentProvider)
+                    {
+                        contentProvider.SetContent(FileHandler.LoadFile(path, contentTypeEnum));
+                    }
+                }
+                catch(Exception exception)
+                {
+                    MessageBox.Show(exception.Message, "Error Loading File", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            if (fileDialog.ShowDialog() == true)
+            {
+                string path = fileDialog.FileName;
+                ContentTypeEnum contentTypeEnum = ContentTypeEnum.NOTES;
+                if (tabs.SelectedContent.GetType().Name == "Calculator")
+                {
+                    contentTypeEnum = ContentTypeEnum.CALCULATOR;
+                }
+                try
+                {
+                    if(tabs.SelectedContent is IContentProvider contentProvider)
+                    {
+                        FileHandler.SaveFile(contentProvider.GetContent(), path, contentTypeEnum);
+                    }
+
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message, "Error Loading File", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
